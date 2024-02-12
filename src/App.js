16c8d11999848fc0d13c0871800371d0d3a059e6
Somsurya Mohapatra <somsurya.mohapatra2122@gmail.com>
2024-02-13 01:19:10 +0530

    Popular, Offers, New Collections, News Letter & Footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div>
@@ -20,8 +21,9 @@ function App() {
             <Route path=":productId" element={<Product />} />
           </Route>
           <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<LoginSignup/>}/>
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
-const Item = () => {
+import React from "react";
+import "./Item.css";
+const Item = (props) => {
   return (
-    <div>Item</div>
-  )
-}
+    <div className="item">
+      <img src={props.image} alt="product" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">&#8377;{props.new_price}</div>
+        <div className="item-price-old">&#8377;{props.old_price}</div>
+      </div>
+    </div>
+  );
+};
 
-export default Item
+export default Item;
